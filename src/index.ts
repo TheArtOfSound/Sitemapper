80cf5ca831dc50d4e105cf0c2d8d975dacb25d0c
Bryan Leonard <93687456+TheArtOfSound@users.noreply.github.com>
2026-06-09 10:22:22 -0700

    Add public exports

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { buildSiteIndex } from './build.js';
+export type {
+  IssueSeverity,
+  PageIssue,
+  PageRecord,
+  RawSitemapEntry,
+  SitemapSource,
+  SitemapperOptions,
+  SitemapperResult,
+  SitemapperScores,
+  SitemapperStats
+} from './types.js';
